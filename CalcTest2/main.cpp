9e1e8725d5b16960cf3aa9c9baab1585a28cadb5
Max B <max.a.brod@outlook.de>
2022-10-26 14:46:35 +0200

    - Add batch arguments, add. changes

--- CalcTest2/main.cpp
+++ CalcTest2/main.cpp
@@ -3,8 +3,13 @@
 
 int main()
 {
+	std::system("@echo off");
 	std::system("title CalcTest2 v1.1");
+	std::system("color 1f");
+	std::system("cls");
 	int f {1};
+	userFirstOpen();
 	userTextGen(f);
+	userExitMsg();
 	return 0;
 }
